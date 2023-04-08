80a708196eba923de322111d0a1f43b83edb6ab4
cookiepawn <cookiepawn@naver.com>
2023-04-08 19:53:59 +0900

    UPDATE 1. 로딩 효과 추가 2. 창 크기별로 상품 크기 조정

--- js/objectControl.js
+++ js/objectControl.js
@@ -1,27 +1,56 @@
 var object = document.querySelectorAll('.object');
-var objectWidth = object[0].clientWidth;
+var div = document.querySelector('.objectDiv');
+var category = document.querySelectorAll('.enterDiv');
+var cgDiv = document.querySelector('.categoryDiv');
 
 
-
-
-
-const $target = object[0];
-
-const observer = new ResizeObserver((entries) => {
-  // 관찰 중인 배열 형식의 객체 리스트
-  entries.forEach((entry) => {
-    if(objectWidth >= 250) {
-        for(let i=0; i <= object.length; i++) {
-            object[i].style.width = '17%';
-            object[i].style.margin = '1.5%';
-        }
+window.addEventListener('resize', ()=> {
+  if(div.clientWidth > 1250) {
+    for(let i=0; i <= object.length; i++) {
+        object[i].style.width = '17%';
+        object[i].style.margin = '1.5%';
     }
-  });
-});
+  }
+  else if(div.clientWidth <= 1250 && div.clientWidth > 850) {
+    for(let i=0; i < object.length; i++) {
+      object[i].style.width = '20%';
+      object[i].style.margin = '2.4%';
+    }
+  }
+  else if(div.clientWidth <= 850 && div.clientWidth > 500) {
+    for(let i=0; i < object.length; i++) {
+      object[i].style.width = '30%';
+      object[i].style.margin = '1.6%';
+    }
+  }
+  else if(div.clientWidth <= 500) {
+    for(let i=0; i < object.length; i++) {
+      object[i].style.width = '40%';
+      object[i].style.margin = '5%';
+    }
+  }
 
-// 크기변화를 관찰할 요소지정
-observer.observe($target);
 
+
+
+
+
+
+  
+
+  if(cgDiv.clientWidth <= 140) {
+    for(let i=0; i < category.length; i++) {
+      category[i].style.width = '90%';
+      category[i].style.margin = '5%';
+    }
+  }
+  else if(cgDiv.clientWidth > 140) {
+    for(let i=0; i < category.length; i++) {
+      category[i].style.width = '40%';
+      category[i].style.margin = '5%';
+    }
+  }
+})
 
 
 
