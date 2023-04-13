cd2a1f284b6a35f10722c315c251017a3ce2e530
cookiepawn <cookiepawn@naver.com>
2023-04-13 15:42:03 +0900

    UPDATE 1. 로드 이벤트 추가

--- js/objectControl.js
+++ js/objectControl.js
@@ -4,6 +4,53 @@ var category = document.querySelectorAll('.enterDiv');
 var cgDiv = document.querySelector('.categoryDiv');
 
 
+window.addEventListener('load', ()=> {
+  if(div.clientWidth > 1250) {
+    for(let i=0; i <= object.length; i++) {
+        object[i].style.width = '17%';
+        object[i].style.margin = '1.5%';
+    }
+  }
+  else if(div.clientWidth <= 1250 && div.clientWidth > 850) {
+    for(let i=0; i < object.length; i++) {
+      object[i].style.width = '20%';
+      object[i].style.margin = '2.4%';
+    }
+  }
+  else if(div.clientWidth <= 850 && div.clientWidth > 500) {
+    for(let i=0; i < object.length; i++) {
+      object[i].style.width = '30%';
+      object[i].style.margin = '1.6%';
+    }
+  }
+  else if(div.clientWidth <= 500) {
+    for(let i=0; i < object.length; i++) {
+      object[i].style.width = '40%';
+      object[i].style.margin = '5%';
+    }
+  }
+
+  if(cgDiv.clientWidth <= 140) {
+    for(let i=0; i < category.length; i++) {
+      category[i].style.width = '90%';
+      category[i].style.margin = '5%';
+    }
+  }
+  else if(cgDiv.clientWidth > 140) {
+    for(let i=0; i < category.length; i++) {
+      category[i].style.width = '40%';
+      category[i].style.margin = '5%';
+    }
+  }
+
+
+
+
+})
+
+
+
+
 window.addEventListener('resize', ()=> {
   if(div.clientWidth > 1250) {
     for(let i=0; i <= object.length; i++) {
